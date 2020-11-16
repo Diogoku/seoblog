36ae79efdb4f0212a9010fa198c72265b40c013d
diogoku <diogomnribeiro@gmail.com>
2020-11-16 02:07:11 +0000

    signing api url change

--- actions/auth.js
+++ actions/auth.js
@@ -43,7 +43,7 @@ export const signup = (user) => {
 
 // signin
 export const signin = (user) => {
-  return fetch(`${API}/signin`, {
+  return fetch(`signin`, {
     method: "POST",
     headers: {
       Accept: "application/json",
